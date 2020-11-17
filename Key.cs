a222e3d1825706b88e6ade3f072eafd824f079e3
pkim1334 <61428736+pkim1334@users.noreply.github.com>
2020-11-16 21:02:59 -0500

    Add files via upload

---
+++ Key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+   [SerializeField] private KeyType keyType;
+   public enum KeyType{
+      Gold
+   }
+   public KeyType GetKeyType(){
+      return keyType;
+   }
+}
